d42493feda7128e2a2daee0e2a9a71b7860d08d3
Artem Jarovoj <artem.jarovoj17@googlemail.com>
2025-10-22 22:56:44 +0200

    add usestate, onchange event and if clauses

--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Hello(props) {
-  return <h1>Hallo {props.name}!</h1>;
+function Hello() {
+  const [name, setName] = useState("");
+
+  // Event-Handler für Eingabe
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <div style={{ margin: "20px" }}>
+      <input
+        type="text"
+        placeholder="Gib deinen Namen ein"
+        value={name}
+        onChange={handleChange}
+        style={{ padding: "5px", marginRight: "10px" }}
+      />
+      <p>Hallo {name ? name : "..."}</p>
+    </div>
+  );
 }
-
 
 export default Hello;
